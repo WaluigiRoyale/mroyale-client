9bad598c952d6ac24787ee85d342fe0aa11c3375
terminalarch <57570117+terminalarch@users.noreply.github.com>
2021-07-06 14:29:09 +0200

    v3.7.2 changes

--- js/server.js
+++ js/server.js
@@ -1,3 +1,3 @@
-const WEBSOCKET_SERVER = "ws://mroyale.cyuubi.xyz:9000/royale/ws"; //todo: use window url
-var DISCLAIMER_SCREEN_TIMEOUT = 5000;
+var WEBSOCKET_SERVER = "wss://marioroyale.tk:9000/royale/ws";
+var DISCLAIMER_SCREEN_TIMEOUT=3230;
 print("loading server.js finished");
--- js/url.js
+++ js/url.js
@@ -1,36 +1,46 @@
-var LOBBY_MUSIC_URL = ASSETS_URL + "audio/music/lobby.mp3";
-var MENU_MUSIC_URL = ASSETS_URL + "audio/music/menu.mp3";
-var SKINCOUNT = 1;
-var SKIN_MUSIC_URL = {};
-var TILE_ANIMATION = {};
-var OBJ_ANIMATION = {};
-(function() {
-    var assetData = resources["assets.json"];
-    if (assetData.skins.count != undefined)
-        SKINCOUNT=assetData.skins.count;
-    for (i in assetData.skins.properties) {
-        var prop = assetData.skins.properties;
-        var music = prop[i].music;
-        if (music != undefined)
-            SKIN_MUSIC_URL[prop[i].id] = music;
-    }
-    if (assetData.tileAnim) {
-        for (var anim of assetData.tileAnim) {
-            var obj = {};
-            obj.tiles = anim.tiles;
-            obj.delay = anim.delay;
-            obj.tilesets = anim.tilesets || [];
-            TILE_ANIMATION[anim.startTile] = obj;
+{
+
+    var LOBBY_MUSIC_URL = ASSETS_URL + "audio/music/lobby.mp3";
+    var MENU_MUSIC_URL = ASSETS_URL + "audio/music/menu.mp3";
+    var SKINCOUNT = 1;
+    var SKIN_MUSIC_URL = {};
+    var TILE_ANIMATION = {};
+    var OBJ_ANIMATION = {};
+    var GUEST_SKINS = [];
+    (function() {
+        var assetData = resources[ASSETS_URL + "assets.json"];
+        if (assetData.skins.count != undefined) {
+            SKINCOUNT = assetData.skins.count;
+        } else {
+            SKINCOUNT = 1201;
+        }
+        for (i in assetData.skins.properties) {
+            var prop = assetData.skins.properties;
+            var music = prop[i].music;
+            if (music != undefined)
+                SKIN_MUSIC_URL[prop[i].id] = music;
+            if (prop[i].forGuests)
+                GUEST_SKINS.push(prop[i].id);
+        }
+        if (assetData.tileAnim) {
+            for (var anim of assetData.tileAnim) {
+                var obj = {};
+                obj.tiles = anim.tiles;
+                obj.delay = anim.delay;
+                obj.tilesets = anim.tilesets || [];
+                TILE_ANIMATION[anim.startTile] = obj;
+            }
+        }
+        if (assetData.objAnim) {
+            for (var anim of assetData.objAnim) {
+                var obj = {};
+                obj.tiles = anim.tiles;
+                obj.delay = anim.delay;
+                obj.tilesets = anim.tilesets || [];
+                OBJ_ANIMATION[anim.startTile] = obj;
+            }
         }
     }
-    if (assetData.objAnim) {
-        for (var anim of assetData.objAnim) {
-            var obj = {};
-            obj.tiles = anim.tiles;
-            obj.delay = anim.delay;
-            obj.tilesets = anim.tilesets || [];
-            OBJ_ANIMATION[anim.startTile] = obj;
-        }
-    }
-})();
-print("loading url.js finished");
+    )();
+    print("loading url.js finished");
+}
